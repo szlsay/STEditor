55c553fc0dbf402b2989bd97f4bd92d5a53d111f
shenzhaoliang <409178030@qq.com>
2024-03-18 21:53:16 +0800

    element

--- steditor/main.js
+++ steditor/main.js
@@ -4,29 +4,12 @@ import plugin from './js_sdk/uni-admin/plugin'
 import messages from './i18n/index.js'
 
 const lang = uni.getLocale()
-// #ifndef VUE3
-import Vue from 'vue'
-import VueI18n from 'vue-i18n'
-Vue.config.productionTip = false
-Vue.use(VueI18n)
-// 通过选项创建 VueI18n 实例
-const i18n = new VueI18n({
-  locale: lang, // 设置地区
-  messages, // 设置地区信息
-})
-Vue.use(plugin)
-App.mpType = 'app'
-const app = new Vue({
-  i18n,
-  store,
-  ...App
-})
-app.$mount()
-// #endif
-
-// #ifdef VUE3
 import { createSSRApp } from 'vue'
 import { createI18n } from 'vue-i18n'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 export function createApp() {
   const app = createSSRApp(App)
   const i18n = createI18n({
@@ -36,8 +19,9 @@ export function createApp() {
   app.use(i18n)
   app.use(plugin)
   app.use(store)
+	app.use(ElementPlus)
   return {
     app
   }
 }
-// #endif
+
