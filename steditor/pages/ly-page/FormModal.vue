b860f7e34611059ebc912c0e31a0446968d9ec1e
shenzhaoliang <409178030@qq.com>
2024-03-19 22:46:11 +0800

    form

--- steditor/pages/ly-page/FormModal.vue
+++ steditor/pages/ly-page/FormModal.vue
@@ -1,6 +1,23 @@
 <template>
-	<el-dialog v-model="dialogVisible" title="模版编辑" width="500">
-		123
+	<el-dialog v-model="dialogVisible" title="模版编辑" width="50%">
+		{{formData}}
+		<el-form :model="formData" label-width="auto" style="max-width: 600px">
+			<el-form-item label="标题">
+				<el-input v-model="formData.title" />
+			</el-form-item>
+			<el-form-item label="JSON数据字符串">
+				<el-input v-model="formData.json" />
+			</el-form-item>
+			<el-form-item label="自定义组件">
+				<el-input v-model="formData.component" />
+			</el-form-item>
+			<el-form-item label="创建时间">
+				<el-date-picker v-model="formData.create_time" type="datetime" value-format="x" />
+			</el-form-item>
+			<el-form-item label="更新时间">
+				<el-date-picker v-model="formData.update_time" type="datetime" value-format="x" />
+			</el-form-item>
+		</el-form>
 	</el-dialog>
 </template>
 
@@ -8,7 +25,14 @@
 	export default {
 		data() {
 			return {
-				dialogVisible: false
+				dialogVisible: false,
+				formData: {
+					"title": null,
+					"json": null,
+					"component": null,
+					"create_time": null,
+					"update_time": null
+				}
 			}
 		},
 		methods: {
--- steditor/pages/ly-page/edit.vue
+++ steditor/pages/ly-page/edit.vue
@@ -1,136 +1,139 @@
 <template>
-  <view class="uni-container">
-    <uni-forms ref="form" :model="formData" validateTrigger="bind">
-      <uni-forms-item name="title" label="标题">
-        <uni-easyinput placeholder="标题" v-model="formData.title" trim="both"></uni-easyinput>
-      </uni-forms-item>
-      <uni-forms-item name="json" label="JSON数据字符串">
-        <uni-easyinput placeholder="JSON数据字符串" v-model="formData.json" trim="both"></uni-easyinput>
-      </uni-forms-item>
-      <uni-forms-item name="component" label="自定义组件">
-        <uni-easyinput placeholder="自定义组件" v-model="formData.component" trim="both"></uni-easyinput>
-      </uni-forms-item>
-      <uni-forms-item name="create_time" label="创建时间">
-        <uni-datetime-picker return-type="timestamp" v-model="formData.create_time"></uni-datetime-picker>
-      </uni-forms-item>
-      <uni-forms-item name="update_time" label="更新时间">
-        <uni-datetime-picker return-type="timestamp" v-model="formData.update_time"></uni-datetime-picker>
-      </uni-forms-item>
-      <view class="uni-button-group">
-        <button type="primary" class="uni-button" style="width: 100px;" @click="submit">提交</button>
-        <navigator open-type="navigateBack" style="margin-left: 15px;">
-          <button class="uni-button" style="width: 100px;">返回</button>
-        </navigator>
-      </view>
-    </uni-forms>
-  </view>
+	<view class="uni-container">
+		{{formData}}
+		<uni-forms ref="form" :model="formData" validateTrigger="bind">
+			<uni-forms-item name="title" label="标题">
+				<uni-easyinput placeholder="标题" v-model="formData.title" trim="both"></uni-easyinput>
+			</uni-forms-item>
+			<uni-forms-item name="json" label="JSON数据字符串">
+				<uni-easyinput placeholder="JSON数据字符串" v-model="formData.json" trim="both"></uni-easyinput>
+			</uni-forms-item>
+			<uni-forms-item name="component" label="自定义组件">
+				<uni-easyinput placeholder="自定义组件" v-model="formData.component" trim="both"></uni-easyinput>
+			</uni-forms-item>
+			<uni-forms-item name="create_time" label="创建时间">
+				<uni-datetime-picker return-type="timestamp" v-model="formData.create_time"></uni-datetime-picker>
+			</uni-forms-item>
+			<uni-forms-item name="update_time" label="更新时间">
+				<uni-datetime-picker return-type="timestamp" v-model="formData.update_time"></uni-datetime-picker>
+			</uni-forms-item>
+			<view class="uni-button-group">
+				<button type="primary" class="uni-button" style="width: 100px;" @click="submit">提交</button>
+				<navigator open-type="navigateBack" style="margin-left: 15px;">
+					<button class="uni-button" style="width: 100px;">返回</button>
+				</navigator>
+			</view>
+		</uni-forms>
+	</view>
 </template>
 
 <script>
-  import { validator } from '../../js_sdk/validator/ly-page.js';
+	import {
+		validator
+	} from '../../js_sdk/validator/ly-page.js';
 
-  const db = uniCloud.database();
-  const dbCmd = db.command;
-  const dbCollectionName = 'ly-page';
+	const db = uniCloud.database();
+	const dbCmd = db.command;
+	const dbCollectionName = 'ly-page';
 
-  function getValidator(fields) {
-    let result = {}
-    for (let key in validator) {
-      if (fields.includes(key)) {
-        result[key] = validator[key]
-      }
-    }
-    return result
-  }
+	function getValidator(fields) {
+		let result = {}
+		for (let key in validator) {
+			if (fields.includes(key)) {
+				result[key] = validator[key]
+			}
+		}
+		return result
+	}
 
-  
 
-  export default {
-    data() {
-      let formData = {
-        "title": "",
-        "json": "",
-        "component": "",
-        "create_time": null,
-        "update_time": null
-      }
-      return {
-        formData,
-        formOptions: {},
-        rules: {
-          ...getValidator(Object.keys(formData))
-        }
-      }
-    },
-    onLoad(e) {
-      if (e.id) {
-        const id = e.id
-        this.formDataId = id
-        this.getDetail(id)
-      }
-    },
-    onReady() {
-      this.$refs.form.setRules(this.rules)
-    },
-    methods: {
-      
-      /**
-       * 验证表单并提交
-       */
-      submit() {
-        uni.showLoading({
-          mask: true
-        })
-        this.$refs.form.validate().then((res) => {
-          return this.submitForm(res)
-        }).catch(() => {
-        }).finally(() => {
-          uni.hideLoading()
-        })
-      },
 
-      /**
-       * 提交表单
-       */
-      submitForm(value) {
-        // 使用 clientDB 提交数据
-        return db.collection(dbCollectionName).doc(this.formDataId).update(value).then((res) => {
-          uni.showToast({
-            title: '修改成功'
-          })
-          this.getOpenerEventChannel().emit('refreshData')
-          setTimeout(() => uni.navigateBack(), 500)
-        }).catch((err) => {
-          uni.showModal({
-            content: err.message || '请求服务失败',
-            showCancel: false
-          })
-        })
-      },
+	export default {
+		data() {
+			let formData = {
+				"title": "",
+				"json": "",
+				"component": "",
+				"create_time": null,
+				"update_time": null
+			}
+			return {
+				formData,
+				formOptions: {},
+				rules: {
+					...getValidator(Object.keys(formData))
+				}
+			}
+		},
+		onLoad(e) {
+			if (e.id) {
+				const id = e.id
+				this.formDataId = id
+				this.getDetail(id)
+			}
+		},
+		onReady() {
+			this.$refs.form.setRules(this.rules)
+		},
+		methods: {
 
-      /**
-       * 获取表单数据
-       * @param {Object} id
-       */
-      getDetail(id) {
-        uni.showLoading({
-          mask: true
-        })
-        db.collection(dbCollectionName).doc(id).field("title,json,component,create_time,update_time").get().then((res) => {
-          const data = res.result.data[0]
-          if (data) {
-            this.formData = data
-            
-          }
-        }).catch((err) => {
-          uni.showModal({
-            content: err.message || '请求服务失败',
-            showCancel: false
-          })
-        }).finally(() => {
-          uni.hideLoading()
-        })
-      }
-    }
-  }
+			/**
+			 * 验证表单并提交
+			 */
+			submit() {
+				uni.showLoading({
+					mask: true
+				})
+				this.$refs.form.validate().then((res) => {
+					return this.submitForm(res)
+				}).catch(() => {}).finally(() => {
+					uni.hideLoading()
+				})
+			},
+
+			/**
+			 * 提交表单
+			 */
+			submitForm(value) {
+				// 使用 clientDB 提交数据
+				return db.collection(dbCollectionName).doc(this.formDataId).update(value).then((res) => {
+					uni.showToast({
+						title: '修改成功'
+					})
+					this.getOpenerEventChannel().emit('refreshData')
+					setTimeout(() => uni.navigateBack(), 500)
+				}).catch((err) => {
+					uni.showModal({
+						content: err.message || '请求服务失败',
+						showCancel: false
+					})
+				})
+			},
+
+			/**
+			 * 获取表单数据
+			 * @param {Object} id
+			 */
+			getDetail(id) {
+				uni.showLoading({
+					mask: true
+				})
+				db.collection(dbCollectionName).doc(id).field("title,json,component,create_time,update_time").get().then((
+				res) => {
+					const data = res.result.data[0]
+					if (data) {
+						this.formData = data
+
+					}
+				}).catch((err) => {
+					uni.showModal({
+						content: err.message || '请求服务失败',
+						showCancel: false
+					})
+				}).finally(() => {
+					uni.hideLoading()
+				})
+			}
+		}
+	}
 </script>
