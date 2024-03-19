c27042e17754942f5ffff59d1d637fd0da0ae72d
shenzhaoliang <409178030@qq.com>
2024-03-19 22:26:40 +0800

    modal

---
+++ steditor/pages/ly-page/FormModal.vue
@@ -0,0 +1,23 @@
+<template>
+	<el-dialog v-model="dialogVisible" title="模版编辑" width="500">
+		123
+	</el-dialog>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				dialogVisible: false
+			}
+		},
+		methods: {
+			onAdd() {
+				this.onEdit()
+			},
+			onEdit() {
+				this.dialogVisible = true
+			}
+		}
+	}
+</script>
